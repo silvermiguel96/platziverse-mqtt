f756c5b7d4118569643e101ac68a3c56d88955fd
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-12-14 16:55:45 -0500

    Cómo recibir mensajes

--- server.js
+++ server.js
@@ -15,9 +15,33 @@ const settings = {
   port: 1883,
   backend
 }
-
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  // Topic tipo de mensaje
+  debug(`Received: ${packet.topic}`)
+  // 
+  debug(`Payload: ${packet.payload}`)
+})
 server.on('ready', () => {
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running `)
 })
+
+server.on('error' , handleFatalError)
+
+function handleFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
