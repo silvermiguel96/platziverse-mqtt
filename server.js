71398e4af49a22512aa159f7838837f648c553dc
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-12-15 21:39:38 -0500

    Cómo almacenar la información del agente y reportar el agente conectado

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
-
+const { parsePayload } = require('./utils')
 const backend = {
   type: 'redis',
   redis,
@@ -26,22 +26,61 @@ const config = {
   operatorsAliases: false
 }
 const server = new mosca.Server(settings)
-
-let Agent, Metric 
+const clients = new Map()
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   // Topic tipo de mensaje
   debug(`Received: ${packet.topic}`)
   // Payload
-  debug(`Payload: ${packet.payload}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+        debug(`Agent  ${agent.uuid} saved`)
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connecte
+              }
+            })
+          })
+        }
+      }
+      break
+  }
 })
 server.on('ready', async () => {
   const services = await db(config).catch(handleFatalError)
@@ -57,6 +96,11 @@ function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
