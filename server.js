da15c77c7a3317a5bb18939e6dff2612b5e89027
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-12-17 23:02:31 -0500

    Cómo almacenar la información de las métricas

--- server.js
+++ server.js
@@ -34,8 +34,31 @@ server.on('clientConnected', client => {
   clients.set(client.id, null)
 })
 
-server.on('clientDisconnected', client => {
+server.on('clientDisconnected', async client => {
   debug(`Client Disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+
+  if (agent) {
+    // Mark Agent as Disconnected
+    agent.connected = false
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (e) {
+      return handleError(e)
+    }
+    // Delete Agent from Clientes List
+    clients.delete(client.id)
+
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+    debug(` Client (${client.id}) associated to Agent (${agent.uuid}) marked as disconnected `)
+  }
 })
 
 server.on('published', async (packet, client) => {
@@ -73,10 +96,21 @@ server.on('published', async (packet, client) => {
                 name: agent.name,
                 hostname: agent.hostname,
                 pid: agent.pid,
-                connected: agent.connecte
+                connected: agent.connected
               }
             })
           })
+        }
+        // Store Metric
+        for (let metric of payload.metrics) {
+          let m
+
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
         }
       }
       break
